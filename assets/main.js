d8748787db5107f9be12a91b1e3e6a2d13f150b4
Leah English <leaheenglish10@gmail.com>
2026-05-03 11:00:39 -0500

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -20,7 +20,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 
 // transitioning images for main slideshow
-const slides = document.querySelectorAll('.slide');
+/* const slides = document.querySelectorAll('.slide');
 let currentIndex = 0;
 
 function showNextSlide() {
@@ -35,4 +35,25 @@ function showNextSlide() {
 }
 
 // Change image every 3 seconds
-setInterval(showNextSlide, 1000);
+setInterval(showNextSlide, 1000); */
+
+
+// testing
+const images = [
+  "../images/slideshowFourths/snapshot_000.png",
+  "../images/slideshowFourths/snapshot_060.png",
+  "../images/slideshowFourths/snapshot_120.png",
+  "../images/slideshowFourths/snapshot_180.png",
+  "../images/slideshowFourths/snapshot_240.png"
+];
+
+let index = 0;
+const slideshow = document.getElementById("slideshow");
+
+function showNextImage() {
+  index = (index + 1) % images.length; // loops back to 0
+  slideshow.src = images[index];
+}
+
+// change image every 1 second (1000 ms)
+setInterval(showNextImage, 1000);
